a54b4bf0f9e6353fbdd7d1dba14d77bdc2b36a27
Aviraj Sharma <aviraj.sharma@mushroomworldgroup.com>
2025-11-05 17:27:18 +0530

    fixed manifest

--- app/layout.js
+++ app/layout.js
@@ -46,16 +46,15 @@ export default function RootLayout({ children }) {
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover" />
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#1A2A5A" />
-
+        <meta name="theme-color" content="#111827" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="Talio HRMS" />
         <meta name="application-name" content="Talio HRMS" />
-        <meta name="msapplication-TileColor" content="#1A2A5A" />
+        <meta name="msapplication-TileColor" content="#111827" />
         <meta name="msapplication-TileImage" content="/icons/icon-144x144.png" />
-        <meta name="msapplication-navbutton-color" content="#1A2A5A" />
+        <meta name="msapplication-navbutton-color" content="#111827" />
         <link rel="apple-touch-icon" sizes="152x152" href="/icons/icon-152x152.png" />
         <link rel="apple-touch-icon" sizes="192x192" href="/icons/icon-192x192.png" />
         <link rel="icon" type="image/png" sizes="192x192" href="/icons/icon-192x192.png" />
